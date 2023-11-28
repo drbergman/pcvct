c4f4fce1f12ff1ffe7e48856da00c7c75292f44e
Daniel Bergman <danielrbergman@gmail.com>
2023-11-28 13:18:47 -0500

    updated .gitignore

---
+++ src/VCTDatabase.jl
@@ -0,0 +1,98 @@
+module VCTDatabase
+using SQLite, Tables, DataFrames
+
+home_dir = cd(pwd,homedir())
+db = SQLite.DB()
+
+function initializeDatabase(path_to_database::String)
+    VCTDatabase.db = SQLite.DB(path_to_database)
+    return createSchema()
+end
+
+function initializeDatabase()
+    VCTDatabase.db = SQLite.DB()
+    return createSchema()
+end
+
+function createSchema()
+    SQLite.execute(db, "CREATE TABLE IF NOT EXISTS patients (
+        patient_id INTEGER PRIMARY KEY,
+        patient_name TEXT
+        )
+    ")
+
+    SQLite.execute(db, "CREATE TABLE IF NOT EXISTS cohorts (
+        cohort_id INTEGER PRIMARY KEY,
+        intervention TEXT UNIQUE
+        )
+    ")
+
+    control_cohort_id = DBInterface.execute(db, "INSERT OR IGNORE INTO cohorts (cohort_id, intervention) VALUES (0, 'none') RETURNING cohort_id;") |> DataFrame |> x->x.cohort_id
+    if isempty(control_cohort_id)
+        control_cohort_id = DBInterface.execute(db, "SELECT cohort_id FROM cohorts WHERE intervention='none';") |> DataFrame |> x->x.cohort_id
+    end
+    control_cohort_id = control_cohort_id[1]
+
+    SQLite.execute(db, "CREATE TABLE IF NOT EXISTS folders (
+        folder_id INTEGER PRIMARY KEY,
+        patient_id INTEGER,
+        cohort_id INTEGER,
+        path TEXT UNIQUE,
+        FOREIGN KEY (patient_id)
+            REFERENCES patients (patient_id),
+        FOREIGN KEY (cohort_id)
+            REFERENCES cohorts (cohort_id),
+        UNIQUE(patient_id,cohort_id)
+        )
+    ")
+
+    SQLite.execute(db, "CREATE TABLE IF NOT EXISTS simulations (
+        simulation_id INTEGER PRIMARY KEY,
+        patient_id INTEGER,
+        variation_id INTEGER,
+        cohort_id INTEGER,
+        folder_id INTEGER,
+        FOREIGN KEY (patient_id)
+            REFERENCES patients (patient_id),
+        FOREIGN KEY (cohort_id)
+            REFERENCES cohorts (cohort_id)
+        FOREIGN KEY (folder_id)
+            REFERENCES folders (folder_id)
+        )    
+    ")
+
+    SQLite.execute(db, "CREATE TABLE IF NOT EXISTS trials (
+        trial_id INTEGER PRIMARY KEY,
+        datetime TEXT,
+        description TEXT
+        )
+    ")
+    return db, control_cohort_id
+end
+
+function selectRow(table_name::String, condition_stmt::String)
+    s = "SELECT * FROM $(table_name) " * condition_stmt * ";"
+    df = DBInterface.execute(db, s) |> DataFrame
+    @assert size(df,1)==1 "Did not find exactly one row matching the query:\n\tDatabase file: $(db)\n\tQuery: $(s)\nResult: $(df)"
+    return df
+end
+
+function selectRow(column_names::Vector{String}, table_name::String, condition_stmt::String)
+    df = selectRow(table_name, condition_stmt)
+    return [df[1,column_name] for column_name in column_names]
+end
+
+function selectRow(column_name::String, table_name::String, condition_stmt::String)
+    df = selectRow(table_name, condition_stmt)
+    return df[1,column_name]
+end
+
+function getFolderID(patient_id::Int,cohort_id::Int)
+    return selectRow("folder_id","folders","WHERE patient_id=$(patient_id) AND cohort_id=$(cohort_id)")
+end
+
+function retrieveFolderInfo(folder_id::Int)
+    return selectRow("folders","WHERE folder_id=$(folder_id)")
+end
+
+end
---
+++ src/VCTExtraction.jl
@@ -0,0 +1,202 @@
+module VCTExtraction
+
+export loadTimeTimeSeries, loadCellCountTimeSeries, loadCellDataTimeSeries
+
+using MAT, LightXML, SQLite, DataFrames, CSV
+
+home_dir = cd(pwd,homedir())
+data_dir = "./data"
+
+include("./VCTConfiguration.jl")
+include("./VCTDatabase.jl")
+
+db, ~ = VCTDatabase.initializeDatabase()
+
+function loadVCT(path_to_data)
+    VCTExtraction.data_dir = path_to_data
+    VCTExtraction.db, _ = VCTDatabase.initializeDatabase(path_to_data * "/vct.db")
+end
+
+############# Helper functions #############
+
+function grabOutputFiles(path_to_output_folder::String, filename_suffix::String)
+    return path_to_output_folder .* ["initial" * filename_suffix
+        readdir(path_to_output_folder) |> x -> filter!(endswith(filename_suffix), x) |> x -> filter!(startswith("output"), x)
+        "final" * filename_suffix]
+end
+
+function getLabelIndex(path_to_xml::String, label::String)
+    xml_path = ["cellular_information", "cell_populations", "cell_population", "custom", "simplified_data", "labels"]
+    VCTConfiguration.openXML(path_to_xml)
+    labels_element = VCTConfiguration.retrieveElement(xml_path)
+    for label_element in child_elements(labels_element)
+        if content(label_element) == label
+            label_ind_start = attribute(label_element, "index"; required=true) |> x -> parse(Int, x)
+            label_ind_width = attribute(label_element, "size"; required=true) |> x -> parse(Int, x)
+            VCTConfiguration.closeXML()
+            return label_ind_start .+ (1:label_ind_width)
+        end
+    end
+    VCTConfiguration.closeXML()
+end
+
+function getSubstrateID(path_to_xml::String, name::String)
+    xml_path = ["microenvironment","domain","variables"]
+    VCTConfiguration.openXML(path_to_xml)
+    variables_element = VCTConfiguration.retrieveElement(xml_path)
+    for variable_element in child_elements(variables_element)
+        if attribute(variable_element, "name"; required=true)==name
+            substrate_id = attribute(variable_element, "ID"; required=true) |> x->parse(Int,x)
+            VCTConfiguration.closeXML()
+            return substrate_id
+        end
+    end
+    VCTConfiguration.closeXML()
+end
+
+function selectSimulations(; patient_id::Int, variation_id::Int, cohort_id::Int)
+    return DBInterface.execute(db, "SELECT simulation_id FROM simulations WHERE (patient_id, variation_id, cohort_id)=($(patient_id),$(variation_id),$(cohort_id));") |> DataFrame |> x -> x.simulation_id
+end
+
+function selectTrialSimulations(trial_id::Int)
+    path_to_trial = VCTExtraction.data_dir * "/trials/" * string(trial_id) * "/"
+    df = CSV.read(path_to_trial*"simulations.csv",DataFrame; header=false,silencewarnings=true,types=String,delim=",")
+    simulation_ids = Int[]
+    for i in axes(df,1)
+        s = df.Column1[i]
+        I = split(s,":") .|> string .|> x->parse(Int,x)
+        if length(I)==1
+            push!(simulation_ids,I[1])
+        else
+            append!(simulation_ids,I[1]:I[2])
+        end
+    end
+    return simulation_ids
+end
+
+############# Atomic extraction functions #############
+
+function extractTime(path_to_output_file::String)
+    xml_path = ["metadata", "current_time"]
+    VCTConfiguration.openXML(path_to_output_file)
+    t = VCTConfiguration.getField(xml_path) |> x -> parse(Float64, x)
+    VCTConfiguration.closeXML()
+    return t
+end
+
+function extractCellCount(path_to_output_file::String)
+    return matread(path_to_output_file)["cell"] |> x -> size(x, 2)
+end
+
+function extractCellData(path_to_output_file::String, label_ind::UnitRange{Int})
+    return matread(path_to_output_file)["cell"][label_ind, :]
+end
+
+function extractCellTypeCount(path_to_output_file::String, cell_type_id::Vector{Int}, label_ind::UnitRange{Int})
+    return extractCellData(path_to_output_file, label_ind) |> vec |> (x -> x .== (cell_type_id')) |> x -> sum(x; dims=1)
+end
+
+function extractCellTypeCount(path_to_output_file::String, cell_type_id::Int, label_ind::UnitRange{Int})
+    return extractCellTypeCount(path_to_output_file, [cell_type_id], label_ind)[1]
+end
+
+function extractSubstrateDensity(path_to_output_file::String, substrate_id::Vector{Int})
+    return matread(path_to_output_file)["multiscale_microenvironmen"][5 .+ substrate_id, :]::Matrix{Float64} # (substrates,voxels)
+end
+
+function extractSubstrateDensity(path_to_output_file::String, substrate_id::Int)
+    return extractSubstrateDensity(path_to_output_file, [substrate_id])
+end
+
+############# Whole simulation loading functions #############
+
+function loadTimeTimeSeries(path_to_output_folder::String)
+    files = grabOutputFiles(path_to_output_folder, ".xml")
+    return [extractTime(file) for file in files]
+end
+
+function loadTimeTimeSeries(path_to_output_folders::Vector{String})
+    return [loadTimeTimeSeries(path_to_output_folder) for path_to_output_folder in path_to_output_folders]
+end
+
+function loadTimeTimeSeries(simulation_id::Union{Int,Vector{Int}})
+    return loadTimeTimeSeries([VCTExtraction.data_dir*"/simulations/$d/output/" for d in simulation_id])
+end
+
+function loadTimeTimeSeries(patient_id::Int, variation_id::Int, cohort_id::Int)
+    return selectSimulations(; patient_id, variation_id, cohort_id) |> loadTimeTimeSeries
+end
+
+function loadCellCountTimeSeries(path_to_output_folder::String)
+    files = grabOutputFiles(path_to_output_folder, "_cells.mat")
+    return [extractCellCount(file) for file in files]
+end
+
+function loadCellCountTimeSeries(path_to_output_folders::Vector{String})
+    return [loadCellCountTimeSeries(path_to_output_folder) for path_to_output_folder in path_to_output_folders]
+end
+
+function loadCellCountTimeSeries(simulation_id::Union{Int,Vector{Int}})
+    return loadCellCountTimeSeries([VCTExtraction.data_dir*"/simulations/$d/output/" for d in simulation_id])
+end
+
+function loadCellCountTimeSeries(patient_id::Int, variation_id::Int, cohort_id::Int)
+    return selectSimulations(; patient_id, variation_id, cohort_id) |> loadCellCountTimeSeries
+end
+
+function loadCellDataTimeSeries(path_to_output_folder::String, label::String)
+    files = grabOutputFiles(path_to_output_folder, "_cells.mat")
+    label_ind = getLabelIndex(path_to_output_folder * "initial.xml", label)
+    return [extractCellData(file, label_ind) for file in files]
+end
+
+function loadCellDataTimeSeries(label::String)
+    simulation_ids = DBInterface.execute(db, "SELECT simulation_id FROM simulations;") |> DataFrame |> x->x.simulation_id
+    paths_to_output_folder = (VCTExtraction.data_dir * "/simulations/") .* (string.(simulation_ids) .* "/output/")
+    all_cell_data = [loadCellDataTimeSeries(path_to_output_folder, label) for path_to_output_folder in paths_to_output_folder]
+    return DataFrame("simulation_id" => simulation_ids, label => all_cell_data)
+end
+
+function loadCellDataTimeSeries(trial_id::Int, label::String)
+    simulation_ids = selectTrialSimulations(trial_id)
+    return DataFrame("simulation_id" => simulation_ids, label => [loadCellDataTimeSeries(VCTExtraction.data_dir * "/simulations/" * string(simulation_id) * "/output/", label) for simulation_id in simulation_ids])
+end
+
+function loadCellTypeCountTimeSeries(path_to_output_folder::String, cell_type_id::Union{Int,Vector{Int}})
+    files = grabOutputFiles(path_to_output_folder, "_cells.mat")
+    label_ind = getLabelIndex(path_to_output_folder * "initial.xml", "cell_type")
+    return [extractCellTypeCount(file, cell_type_id, label_ind) for file in files] |> x -> cat(x...; dims=1)
+end
+
+function loadCellTypeCountTimeSeries(path_to_output_folders::Vector{String}, cell_type_id::Union{Int,Vector{Int}})
+    return [loadCellTypeCountTimeSeries(path_to_output_folder, cell_type_id) for path_to_output_folder in path_to_output_folders]
+end
+
+function loadCellTypeCountTimeSeries(simulation_id::Union{Int,Vector{Int}}, cell_type_id::Union{Int,Vector{Int}})
+    return loadCellTypeCountTimeSeries([VCTExtraction.data_dir*"/simulations/$d/output/" for d in simulation_id], cell_type_id)
+end
+
+function loadCellTypeCountTimeSeries(patient_id::Int, variation_id::Int, cohort_id::Int, cell_type_id::Union{Int,Vector{Int}})
+    return selectSimulations(; patient_id, variation_id, cohort_id) |> x -> loadCellTypeCountTimeSeries(x, cell_type_id)
+end
+
+function loadSubstrateDensityTimeSeries(path_to_output_folder::String, substrate_id::Union{Int,Vector{Int}})
+    files = grabOutputFiles(path_to_output_folder, "_microenvironment0.mat")
+    return [extractSubstrateDensity(file, substrate_id) for file in files] |> x -> cat(x...; dims=3) |> x -> permutedims(x, (3, 2, 1))::Array{Float64,3} # (time,voxel,substrate)
+end
+
+function loadSubstrateDensityTimeSeries(path_to_output_folders::Vector{String}, substrate_id::Union{Int,Vector{Int}})
+    return [loadSubstrateDensityTimeSeries(path_to_output_folder,substrate_id) for path_to_output_folder in path_to_output_folders]
+end
+
+function loadSubstrateDensityTimeSeries(path_to_output_folders::Union{String,Vector{String}}, substrate_names::Vector{String})
+    path_to_xmls = path_to_output_folders .* "initial.xml"
+    substrate_ids = [[getSubstrateID(path_to_xml, substrate_name) for substrate_name in substrate_names] for path_to_xml in path_to_xmls] # I no IT shud Bee paths_to_xml butt lettuce just ad esses & B kühl, yeah?
+    return [loadSubstrateDensityTimeSeries(path_to_output_folder,substrate_id) for (path_to_output_folder,substrate_id) in zip(path_to_output_folders, substrate_ids)]
+end
+
+function loadSubstrateDensityTimeSeries(path_to_output_folders::Union{String,Vector{String}}, substrate_name::String)
+    return loadSubstrateDensityTimeSeries(path_to_output_folders, [substrate_name])
+end
+
+end
