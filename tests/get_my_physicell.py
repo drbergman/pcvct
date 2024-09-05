51ff535c73c1bc7b8f4312d8fdc879d4e041a825
Daniel Bergman <danielrbergman@gmail.com>
2024-09-04 20:30:05 -0400

    start testing

---
+++ tests/get_my_physicell.py
@@ -0,0 +1,16 @@
+# Python script to download the latest release of PhysiCell
+
+import requests
+
+branch = "my-physicell"
+
+response = requests.get(f"https://api.github.com/repos/drbergman/PhysiCell/branches/{branch}")
+print(response)
+release_name_str = response.json()["name"]
+
+remote_url = f"https://github.com/drbergman/PhysiCell/archive/refs/heads/{branch}.zip"
+print("remote_url=",remote_url)
+local_file = f"PhysiCell-{branch}.zip"
+data = requests.get(remote_url)
+with open(local_file, 'wb')as file:
+  file.write(data.content)
